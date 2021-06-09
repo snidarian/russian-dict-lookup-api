f5acf4b60c7e4626a536bd77edf0fb55b97f6c2a
snidarian <cephalopod31956@gmail.com>
2021-06-09 14:47:09 -0400

    in dev

--- dict-russian.py
+++ dict-russian.py
@@ -6,17 +6,19 @@ import requests as rq
 import json as j
 
 
-parser = ap.ArgumentParser(description="Uses dictionaryapi to define Russian words in Russian")
+parser = ap.ArgumentParser(description="Uses api.dictionaryapi to define Russian words in Russian")
 
 
 parser.add_argument("query", help="Dict term for definition", type=str)
 
 
+args = parser.parse_args()
 
-result = rq.get('https://api.dictionaryapi.dev/api/v2/entries/ru/рот')
+
+result = rq.get(f'https://api.dictionaryapi.dev/api/v2/entries/ru/{args.query}')
 
 
-#print(result.text)
+print(result.text)
 
 
 json_object = result.json()
