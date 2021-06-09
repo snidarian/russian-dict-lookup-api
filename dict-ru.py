8fd9ac5bd623d1981006fc4f1f586f829c8b3495
snidarian <cephalopod31956@gmail.com>
2021-06-09 15:21:05 -0400

    in dev

--- dict-ru.py
+++ dict-ru.py
@@ -9,23 +9,28 @@ import json as j
 parser = ap.ArgumentParser(description="Uses api.dictionaryapi to define Russian words in Russian")
 
 
-parser.add_argument("query", help="Dict term for definition", type=str)
+# parser.add_argument("query", help="Dict term for definition", type=str)
 
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
 
-result = rq.get(f'https://api.dictionaryapi.dev/api/v2/entries/ru/{args.query}')
+# result = rq.get(f'https://api.dictionaryapi.dev/api/v2/entries/ru/{args.query}')
+
+result = rq.get('https://api.dictionaryapi.dev/api/v2/entries/ru/рот')
 
 
-print(result.text)
+json_payload = result.json()
 
 
-json_object = result.json()
+result_dict = json_payload[0]
 
-print(type(json_object))
 
-    
+print(result_dict)
+
+print(type(result_dict))
+
+
 
 
 
