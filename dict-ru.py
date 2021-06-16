3b122877ec4a33cd3e21ea86e7ea940af3a96086
snidarian <cephalopod31956@gmail.com>
2021-06-16 08:38:07 -0400

    program basic function. prints output in prettified json

--- dict-ru.py
+++ dict-ru.py
@@ -9,40 +9,32 @@ import json as j
 parser = ap.ArgumentParser(description="Uses api.dictionaryapi to define Russian words in Russian")
 
 
-# parser.add_argument("query", help="Dict term for definition", type=str)
+parser.add_argument("query", help="Dict term for definition", type=str)
 
 
-# args = parser.parse_args()
+args = parser.parse_args()
 
 
-# result = rq.get(f'https://api.dictionaryapi.dev/api/v2/entries/ru/{args.query}')
-
-result = rq.get('https://api.dictionaryapi.dev/api/v2/entries/ru/рот')
-
+result = rq.get(f'https://api.dictionaryapi.dev/api/v2/entries/ru/{args.query}')
+#result = rq.get('https://api.dictionaryapi.dev/api/v2/entries/ru/рот')
 
 json_payload = result.json()
 
 
-print(type(result))
 print(type(json_payload))
+#print(json_payload)
 
-json_payload = json_payload[0]
+#print(type(result))
+#print(type(json_payload[0]))
 
+try:
+    json_payload = json_payload[0]
+    new_json_object = j.dumps(json_payload, indent=2, ensure_ascii=False)
+    print(new_json_object)
+except KeyError:
+    print("No definition available for that term")
 
-print(json_payload)
-print(json_payload['word'])
-print(json_payload['phonetics'])
-print(json_payload['meanings'])
-meanings_list = json_payload['meanings']
-meanings_dict = meanings_list[0]
+#print(json_payload)
 
-definitions_list = meanings_dict['definitions']
-print()
-
-print(definitions_list)
-
-
-
-
-
-
+#new_json_object = j.dumps(json_payload, indent=2, ensure_ascii=False)
+#print(new_json_object)
