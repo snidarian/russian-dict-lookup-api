7b09700bd8b7fb93778b288d43d35063336de620
snidarian <cephalopod31956@gmail.com>
2021-06-16 06:19:36 -0400

    misc

--- dict-ru.py
+++ dict-ru.py
@@ -26,6 +26,21 @@ json_payload = result.json()
 print(type(result))
 print(type(json_payload))
 
+json_payload = json_payload[0]
+
+
+print(json_payload)
+print(json_payload['word'])
+print(json_payload['phonetics'])
+print(json_payload['meanings'])
+meanings_list = json_payload['meanings']
+meanings_dict = meanings_list[0]
+
+definitions_list = meanings_dict['definitions']
+print()
+
+print(definitions_list)
+
 
 
 
